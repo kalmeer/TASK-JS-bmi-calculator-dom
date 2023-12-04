38b392fa02b5cd73439deadf903e1f1e2cca8833
Khalid <khalidalmeer@gmail.com>
2023-12-04 21:28:57 +0300

    a

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  let height2 = (height / 100) * (height / 100);
+  let BMI = weight / height2;
+  alert(BMI);
+
+  // 1. Below 18.5	Underweight
+  //   2. 18.5 – 24.9	Healthy Weight
+  //   3. 25.0 – 29.9	Overweight
+  //   4. 30.0 and Above	Obesity
+  if (BMI < 18.5) {
+    console.log("Underweight");
+    alert("Underweight");
+  } else if (BMI >= 18.5 && BMI < 25) {
+    console.log("Healthy weight");
+    alert(" Healthy weight");
+  } else if (BMI >= 25 && BMI < 30) {
+    console.log("Overweight");
+    alert("Overweight");
+  } else if (BMI >= 30) {
+    console.log("Obese");
+    alert("Obese");
+  }
 }
